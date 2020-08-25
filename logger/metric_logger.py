cffe0e400e2a2b1d37c520febb16e5096edfd33a
taufikxu <kunxu.thu@gmail.com>
2020-08-25 16:53:11 +0800

    add autosave

--- logger/metric_logger.py
+++ logger/metric_logger.py
@@ -10,27 +10,14 @@ matplotlib.use("agg")
 
 
 class Logger(object):
-    def __init__(self, log_dir="./logs"):
+    def __init__(self, log_dir="./logs", save_interval=10000):
         self.stats = dict()
         self.log_dir = log_dir
+        self.save_interval = save_interval
         self.tb_writter = SummaryWriter(log_dir)
+
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
-
-    def __getfilename(self, name, class_name=None):
-        if class_name is None:
-            outdir = self.log_dir
-            class_name = "ImageVisualization"
-        else:
-            outdir = os.path.join(self.log_dir, class_name)
-
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-        if isinstance(name, str):
-            outfile = os.path.join(outdir, "{}.png".format(name))
-        else:
-            outfile = os.path.join(outdir, "%08d.png" % name)
-        return outfile
 
     def add(self, category, k, v, it):
         if category not in self.stats:
@@ -39,13 +26,32 @@ class Logger(object):
             self.stats[category][k] = []
         self.stats[category][k].append((it, v))
         self.tb_writter.add_scalar("{}/{}".format(category, k), v, it)
+        if it % self.save_interval == 0:
+            self.save()
 
     def addvs(self, category, keyvalue, it):
         for k in keyvalue:
             v = keyvalue[k]
             self.add(category, k, v, it)
+        if it % self.save_interval == 0:
+            self.save()
 
     def add_imgs(self, imgs, name, class_name=None, vrange=None, nrow=10):
+        def __getfilename(self, name, class_name=None):
+            if class_name is None:
+                outdir = self.log_dir
+                class_name = "ImageVisualization"
+            else:
+                outdir = os.path.join(self.log_dir, class_name)
+
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+            if isinstance(name, str):
+                outfile = os.path.join(outdir, "{}.png".format(name))
+            else:
+                outfile = os.path.join(outdir, "%08d.png" % name)
+            return outfile
+
         outfile = self.__getfilename(name, class_name)
         if vrange is None:
             maxv, minv = float(torch.max(imgs)), float(torch.min(imgs))
@@ -73,7 +79,7 @@ class Logger(object):
             prefix += "\n"
         log_func(prefix)
 
-    def save(self, filename="stat.pkl"):
+    def save(self, filename="ModelStats.pkl"):
         filename = os.path.join(self.log_dir, filename)
         with open(filename, "wb") as f:
             pickle.dump(self.stats, f)
